de717a9eb2c6e400024b70f7c786fa5420374cbf
Joshua Hecker <jhecker2001@gmail.com>
2023-10-10 10:03:19 -0500

    Removed the rounding function.

--- tip_calculator/tip_calculator.py
+++ tip_calculator/tip_calculator.py
@@ -6,5 +6,5 @@ tip = 1 + (float(input("What percentage tip would you like to give? 10, 12, or 1
 
 num_people = int(input("How many people to split the bill? "))
 
-payment_per_person = round(bill / num_people * tip, 5)
+payment_per_person = bill / num_people * tip
 print(f"Each person should pay: ${payment_per_person:.2f}")
